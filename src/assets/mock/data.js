83aeb84d527d32663f24a8dc6be91d221750c3e1
Diego Sanchez <dgsanchez@synopsis.ws>
2020-02-21 13:11:08 -0500

    First commit

---
+++ src/assets/mock/data.js
@@ -0,0 +1,43 @@
+export const fruits = [
+    
+    {
+        "name"  :  "Orange",
+        "price" :  "5.00",
+        "stock" :  "10",
+        "img"   :  "https://image.shutterstock.com/image-photo/ripe-orange-isolated-on-white-260nw-606022676.jpg"
+    },
+    {
+        "name"  :  "Apple",
+        "price" :  "3.00",
+        "stock" :  "15",
+        "img"   :  "https://candlekiln.com/wp-content/uploads/2017/06/apple2.jpg"
+    },
+    {
+        "name"  :  "Banana",
+        "price" :  "2.00",
+        "stock" :  "10",
+        "img"   :  "https://previews.123rf.com/images/kulyk/kulyk1507/kulyk150700013/43279121-banana-fruit-close-up-bunch-of-bananas-isolated-on-white-background-qualitative-vector-illustration-.jpg"
+    },
+    {
+        "name"  :  "Coconaut",
+        "price" :  "3.00",
+        "stock" :  "4",
+        "img"   :  "https://cdn.shopify.com/s/files/1/2154/4937/products/Coconut600x600_1_a00f8f32-80d0-47e5-b70f-4751bbe28d30_600x.png?v=1571578092"
+    },
+    {
+        "name"  :  "Strawberry",
+        "price" :  "6.00",
+        "stock" :  "20",
+        "img"   :  "https://libbys.es/wordpress/wp-content/uploads/2018/05/fresas.jpg"
+    },
+    {
+        "name"  :  "Peach",
+        "price" :  "3.00",
+        "stock" :  "33",
+        "img"   :  "https://greenshop.com.ar/wp-content/uploads/sites/9/2016/08/A.2.17-500GR.jpg"
+    }
+
+    
+
+]
+
