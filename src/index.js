3b2fa172cd5cf7190eb3f5497b6572a20cfe2cc5
Diego Sanchez <dgsanchez@synopsis.ws>
2020-03-04 12:18:51 -0500

    My message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
-import 'bootstrap/dist/css/bootstrap.min.css'
-import $ from 'jquery';
-import Popper from 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react'
 import ReactDOM from 'react-dom'
