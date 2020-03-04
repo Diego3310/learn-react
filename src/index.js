14b035f4317cca19b89213693427b985bc3bb7e3
Diego Sanchez <dgsanchez@synopsis.ws>
2020-03-04 12:22:42 -0500

    Update to remote branch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-/* eslint-disable jsx-a11y/alt-text */
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-=======
 import App from './App'
->>>>>>> d9e22959af28c142b2b7db65bdb66a6cc91ff8d8
 import React from 'react'
 import ReactDOM from 'react-dom'
 
