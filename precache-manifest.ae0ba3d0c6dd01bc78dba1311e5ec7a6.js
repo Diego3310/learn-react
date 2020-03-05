9a782e3d199bc1357be7f026563f6ea28043f7c4
Diego Sanchez <dgsanchez@synopsis.ws>
2020-03-05 12:02:00 -0500

    Updates

---
+++ precache-manifest.ae0ba3d0c6dd01bc78dba1311e5ec7a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9980dc6763d852054f0e60210915f665",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "c00013a8b6ffdaf20daf",
+    "url": "/learn-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2e0eb70238bef629e9e4",
+    "url": "/learn-react/static/css/main.80cf9bce.chunk.css"
+  },
+  {
+    "revision": "c00013a8b6ffdaf20daf",
+    "url": "/learn-react/static/js/2.aef8dfcd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/learn-react/static/js/2.aef8dfcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0eb70238bef629e9e4",
+    "url": "/learn-react/static/js/main.dc95c4f7.chunk.js"
+  },
+  {
+    "revision": "00a2fff6528312245103",
+    "url": "/learn-react/static/js/runtime-main.c6b4af9c.js"
+  }
+]);
